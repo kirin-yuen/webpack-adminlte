f2b406d89423d6b62622cf742debebcaca3a8a1d
kirin-yuen <ywl_allen@163.com>
2018-05-27 22:40:57 +0800

    安装所需依赖1

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-const path = require('path');
+const path = require('path'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    webpack = require('webpack');
 
 
 module.exports = {
@@ -25,7 +28,58 @@ module.exports = {
                 test: /.js$/,
                 loader: 'babel-loader',
                 include: [path.resolve(__dirname, 'src')]
+            },{
+                test: /.vue$/,
+                loader: 'vue-loader'
+            },{
+                test: /.less$/,
+                loader: 'less-loader'
+            },{
+                test: /.css$/,
+                loader: ['style-loader', 'css-loader']
+            },{
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader?limit=10000&name=images/[name].[hash:8].[ext]'
             }
+            ,{
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader?limit=10000&name=fonts/[name].[hash:8].[ext]'
+            },{
+                test: /\.(swf|mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: 'url-loader?limit=10000&name=media/[name].[hash:8].[ext]'
+            }]
+        ],
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                "window.jQuery": 'jquery',
+                "window.$": 'jquery'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                title: '视频广场',
+                inject: true, // head -> Cannot find element: #app
+                chunks: ['index'],
+                template: './src/index.html',
+                minify: {
+                    removeComments: true,
+                    collapseWhitespace: false
+                }
+            }),
+            //生成版本信息页面, 在这个页面中自动引用入口 about --> dist/js/about.[chunkhash:8].js
+            //以 src/index.html 这个文件作为模板
+            new HtmlWebpackPlugin({
+                filename: 'about.html',
+                title: '版本信息',
+                inject: true,
+                chunks: ['about'],
+                template: './src/index.html',
+                minify: {
+                    removeComments: true,
+                    collapseWhitespace: false
+                }
+            })
         ]
     }
 }
